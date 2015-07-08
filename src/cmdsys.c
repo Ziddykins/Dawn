cb0591cffe5fac087835da16843e6648fd3b97c6
p512 <point6000x+gh@gmail.com>
2015-07-08 06:45:01 +0200

    Added bare command system implementation
    To be used like this:
    first and always: init_cmdsys();
    then add commands freely: registerCmd(0, ";buy", "<material> <amount>", AL_USER, cmd_buy);
    when done adding commands: finalizeCmdSys(0);
    then use: invokeCmd(0, ";buy", message_struct, CMD_EXEC);
    to execute the command ";buy" or replace CMD_EXEC with CMD_HELP to send information about the command.
    when done, free the system: freeCmdSys(0);
    
    NOTES:
    +please refer to player.h for information on auth_level
    +the first parameter of registerCmd can be a validly created CmdSys from createCmdSys or 0 to use the CmdSys created by init_cmdsys()
    +you can add commands after finalizing, but you'll have to call finalizeCmdSys again before calling invokeCmd
    +I thoroughly recommend not using any of the internal functions outside of cmdsys

---
+++ src/cmdsys.c
@@ -0,0 +1,249 @@
+#include "include/cmdsys.h"
+
+CmdSys commands;
+
+void init_cmdsys() {
+    commands = createCmdSys();
+}
+
+CmdSys createCmdSys() {
+    struct cmdSys * ccs = calloc(1, sizeof *ccs);
+    ccs->flags = CMD_INITIALIZED;
+    return ccs;
+}
+
+/*
+void sample_function(struct Message *msg) {
+    printf("YEAH BITCHES!!\n");
+}
+*/
+
+void freeCmdSys(CmdSys cs) {
+    struct cmdSys * ccs = cs ? cs : commands;
+    if(!cs) {
+        return;
+    }
+
+    free(ccs->hashes);
+
+    //free allocated character strings
+    for(size_t i = 0; i < ccs->len; i++) {
+        free(ccs->cmds[i]);
+        free(ccs->helptexts[i]);
+    }
+
+    free(ccs->cmds);
+    free(ccs->helptexts);
+    free(ccs->auth_levels);
+    free(ccs->fn);
+
+    free(ccs);
+}
+
+void registerCmd(CmdSys cs, char * cmd, char * helptext, int auth_level, void (*fn)(struct Message *msg)) {
+    struct cmdSys * ccs = cs ? cs : commands;
+    if(!cs) {
+        return;
+    }
+    ccs->flags = CMD_REGISTERED;
+
+    if(ccs->capacity == 0) {
+        ccs->hashes = calloc(1, sizeof *ccs->hashes);
+        ccs->cmds = calloc(1, sizeof *ccs->cmds);
+        ccs->helptexts = calloc(1, sizeof *ccs->helptexts);
+        ccs->auth_levels = calloc(1, sizeof *ccs->auth_levels);
+        ccs->fn = calloc(1, sizeof *ccs->fn);
+        ccs->capacity = 1;
+    }
+
+    if(ccs->len >= ccs->capacity) {
+        ccs->capacity *= 2;
+        trimArrays(cs);
+    }
+
+    ccs->hashes[ccs->len] = hashCode(cmd);
+
+    ccs->cmds[ccs->len] = malloc(strlen(cmd)+1);
+    strcpy(ccs->cmds[ccs->len], cmd);
+
+    ccs->helptexts[ccs->len] = malloc(strlen(helptext)+1);
+    strcpy(ccs->helptexts[ccs->len], helptext);
+
+    ccs->auth_levels[ccs->len] = auth_level;
+    ccs->fn[ccs->len] = fn;
+
+    ccs->len++;
+}
+
+void finalizeCmdSys(CmdSys cs) {
+    struct cmdSys * ccs = cs ? cs : commands;
+    if(!ccs) {
+        return;
+    }
+
+    ccs->capacity = ccs->len;
+    trimArrays(cs);
+
+    sortCmds(cs);
+    ccs->flags = CMD_FINALIZED;
+}
+
+void invokeCmd(CmdSys cs, char * cmd, struct Message * msg, int mode) {
+    struct cmdSys * ccs = cs ? cs : commands;
+    assert(ccs && msg && cmd);
+    assert(ccs->flags == CMD_FINALIZED);
+
+    size_t cmdID = getCmdID(cs, cmd);
+    char out[MAX_MESSAGE_BUFFER];
+    if(cmdID == (size_t)(-1)) {
+        if(mode == CMD_EXEC)
+            snprintf(out, MAX_MESSAGE_BUFFER, "PRIVMSG %s :Unknown command. Please use ';help'\r\n", msg->receiver);
+        else
+            snprintf(out, MAX_MESSAGE_BUFFER, "PRIVMSG %s :Command does not exist.\r\n", msg->receiver);
+        addMsg(out, strlen(out));
+        //printf("%s", out);
+    } else {
+        if(mode == CMD_EXEC) {
+            ccs->fn[cmdID](msg);
+        } else if(mode == CMD_HELP) {
+            snprintf(out, MAX_MESSAGE_BUFFER, "PRIVMSG %s :Help for '%s': %s", msg->receiver, ccs->cmds[cmdID], ccs->helptexts[cmdID]);
+            addMsg(out, strlen(out));
+            //printf("%s", out);
+        }
+    }
+}
+
+#define GET_IDX(len, x, y) (len*y+x)
+
+//Internal functions
+void sortCmds(CmdSys cs) {
+    size_t const SHIFT = 4;
+    int64_t const BITMASK = (1<<SHIFT)-1;
+    unsigned short const HASH_BITLEN = 64;
+
+    struct cmdSys * ccs = cs ? cs : commands;
+    assert(ccs);
+    assert(ccs->capacity = ccs->len);
+    if(ccs->len <= 1) {
+        return;
+    }
+
+    size_t * positions = malloc(ccs->len * sizeof *positions);
+    for(size_t i = 0; i < ccs->len; i++) {
+        positions[i] = i;
+    }
+
+    size_t * buckets = malloc(ccs->len * (BITMASK+1) * sizeof *buckets); //actually a 2d array
+    size_t * idx = calloc(BITMASK+1, sizeof *idx);
+
+    size_t bitmask = BITMASK;
+    for(size_t i = 0; i < HASH_BITLEN / SHIFT; i++) {
+
+        for(size_t j = 0; j < ccs->len; j++) {
+            size_t pos = positions[j];
+            size_t bucket = (ccs->hashes[pos] & bitmask) >> (i*SHIFT);
+
+            buckets[ccs->len * bucket + idx[bucket]] = pos;
+            idx[bucket]++;
+        }
+
+        size_t c = 0;
+        for(size_t j = 0; j <= BITMASK; j++) {
+            for(size_t k = 0; k < idx[j]; k++) {
+                positions[c] = buckets[GET_IDX(ccs->len, k, j)];
+                c++;
+            }
+            idx[j] = 0;
+        }
+        bitmask <<= SHIFT;
+    }
+
+    free(buckets);
+    free(idx);
+
+    uint64_t * hashes = calloc(ccs->len, sizeof *ccs->hashes);
+    char ** cmds = calloc(ccs->len, sizeof *ccs->cmds);
+    char ** helptexts = calloc(ccs->len, sizeof *ccs->helptexts);
+    int * auth_levels = calloc(ccs->len, sizeof *ccs->auth_levels);
+    void (*(*fn))(struct Message *msg) = calloc(ccs->len, sizeof *ccs->fn);
+
+    for(size_t i = 0; i < ccs->len; i++) {
+        hashes[i] = ccs->hashes[i];
+        cmds[i] = ccs->cmds[i];
+        helptexts[i] = ccs->helptexts[i];
+        auth_levels[i] = ccs->auth_levels[i];
+        fn[i] = ccs->fn[i];
+    }
+
+    for(size_t i = 0; i < ccs->len; i++) {
+        ccs->hashes[i] = hashes[positions[i]];
+        ccs->cmds[i] = cmds[positions[i]];
+        ccs->helptexts[i] = helptexts[positions[i]];
+        ccs->auth_levels[i] = auth_levels[positions[i]];
+        ccs->fn[i] = fn[positions[i]];
+    }
+
+    free(hashes);
+    free(cmds);
+    free(helptexts);
+    free(auth_levels);
+    free(fn);
+    free(positions);
+}
+
+size_t getCmdID(CmdSys cs, char * cmd) {
+    struct cmdSys * ccs = cs;
+    assert(ccs);
+    uint64_t hash = hashCode(cmd);
+    size_t min = 0, max = ccs->len-1, mid = (size_t)(-1);
+    int valid = 0;
+
+    while(max >= min) {
+        mid = (min+max)/2;
+        if(ccs->hashes[mid] == hash) {
+            valid = 1; break;
+        } else if(ccs->hashes[mid] > hash) {
+            max = mid-1;
+        } else {
+            min = mid+1;
+        }
+    }
+
+    if(valid) {
+        int done = 0;
+        for(size_t iter = mid; !done && iter < ccs->len && ccs->hashes[iter] == hash; iter++) {
+            if(strcmp(cmd, ccs->cmds[iter]) == 0) {
+                return iter;
+            }
+        }
+        for(size_t iter = mid; !done && iter < ccs->len && ccs->hashes[iter] == hash; iter--) {
+            if(strcmp(cmd, ccs->cmds[iter]) == 0) {
+                return iter;
+            }
+        }
+    }
+    return (size_t)-1;
+}
+
+void trimArrays(CmdSys cs) {
+    struct cmdSys * ccs = cs;
+    assert(ccs);
+    assert(ccs->len != 0);
+
+    ccs->hashes = realloc(ccs->hashes, ccs->capacity * sizeof *ccs->hashes);
+    ccs->cmds = realloc(ccs->cmds, ccs->capacity * sizeof *ccs->cmds);
+    ccs->helptexts = realloc(ccs->helptexts, ccs->capacity * sizeof *ccs->helptexts);
+    ccs->auth_levels = realloc(ccs->auth_levels, ccs->capacity * sizeof *ccs->auth_levels);
+    ccs->fn = realloc(ccs->fn, ccs->capacity * sizeof *ccs->fn);
+}
+
+uint64_t hashCode(char * str) {
+    uint64_t hash = 1;
+    uint64_t const prime = 13835058055282163729ull;
+    size_t len = strlen(str);
+    for(size_t i = 0; i < len; i++) {
+        hash += hash * 31 + (unsigned char)(str[i]);
+        hash %= prime;
+    }
+    return hash;
+}
---
+++ src/include/cmdsys.h
@@ -0,0 +1,56 @@
+#ifndef CMDSYS_H_INCLUDED
+#define CMDSYS_H_INCLUDED
+
+#include <inttypes.h> //uint64_t
+#include <stddef.h> //size_t
+#include <assert.h>
+#include "network.h"
+#include "player.h"
+
+struct cmdSys {
+    size_t len, capacity;
+    uint64_t * hashes;
+    char ** cmds;
+    char ** helptexts;
+    int * auth_levels;
+    void (*(*fn))(struct Message *msg); //array of function pointers
+    size_t flags; //alignment + safety checks
+};
+
+//void sample_function(struct Message *msg);
+
+enum cmdSysFlags {
+    CMD_UNUSED = 0,
+    CMD_INITIALIZED = 1<<0,
+    CMD_REGISTERED = 1<<1,
+    CMD_FINALIZED = 1<<2
+};
+
+enum cmdMode {
+    CMD_EXEC,
+    CMD_HELP
+};
+
+typedef void * CmdSys;
+
+extern CmdSys commands;
+
+void init_cmdsys(void);
+
+CmdSys createCmdSys(void);
+void freeCmdSys(CmdSys);
+
+void registerCmd(CmdSys, char * cmd, char * helptext, int auth_level, void (*fn)(struct Message *msg));
+
+void finalizeCmdSys(CmdSys);
+
+void invokeCmd(CmdSys, char * cmd, struct Message * msg, int mode);
+
+//Internal functions
+void sortCmds(CmdSys);
+uint64_t hashCode(char *);
+void trimArrays(CmdSys);
+size_t getCmdID(CmdSys cs, char * cmd);
+
+
+#endif // CMDSYS_H_INCLUDED
